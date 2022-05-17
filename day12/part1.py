c7c490f7f27e49020509004676a1e3440fcf0a74
TavanovRuslan <90765434+TavanovRuslan@users.noreply.github.com>
2022-05-18 01:39:07 +0700

    Add files via upload

---
+++ day12/part1.py
@@ -0,0 +1,18 @@
+with open('input.txt','r') as INPUT:
+    string = INPUT.readline()
+
+for element in string:
+    if element.isdigit() == False and element != '-':
+        string = string.replace(element, '.')
+
+array = []
+new_array = []
+array = string.split('.')
+for element in array:
+    if element != '':
+        new_array.append(int(element))
+
+print(sum(new_array), sep = ' ')
+
+with open('output1.txt','w') as O:
+    O.write(str(sum(new_array)))
---
+++ day12/part2.py
@@ -0,0 +1,22 @@
+def changer(json_file):
+    if type(json_file) == list and json_file != []:
+        return sum(changer(element) for element in json_file)
+    elif type(json_file) == dict and 'red' in json_file.values():
+        return 0
+    elif type(json_file) == str:
+        return 0
+    elif type(json_file) == int:
+        return json_file
+    elif type(json_file) == dict:
+        return changer(list(json_file.values()))
+
+
+import json
+with open('input.txt','r') as INPUT:
+    text = INPUT.readline()
+
+score = changer(json.loads(text))
+print(score)
+
+with open('output2.txt', 'w') as OUTPUT:
+    OUTPUT.write(str(score))
